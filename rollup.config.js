2c0143cbd2a08febb6a20716163d16dab5764427
cjbell <cjbell1988@gmail.com>
2021-05-07 08:40:23 -0400

    fix(packaging): be explicit about rollup modules as externals

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
+import json from "@rollup/plugin-json";
 import pkg from "./package.json";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
@@ -9,8 +10,16 @@ const name = "KnockClient";
 
 export default {
   input: "./src/index.ts",
-  external: [/@babel\/runtime/, /node_modules/],
+  external: [
+    /@babel\/runtime/,
+    "axios",
+    "axios-retry",
+    "eventemitter3",
+    "phoenix",
+    "zustand",
+  ],
   plugins: [
+    json(),
     resolve({ extensions }),
     commonjs(),
     babel({ extensions, include: ["src/**/*"], babelHelpers: "runtime" }),
