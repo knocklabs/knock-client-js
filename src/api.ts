0276e47f76dec7d6743999db3fe9396eee037a0c
Connor Lindsey <cblindsey3@gmail.com>
2023-11-16 14:00:12 -0800

    chore: update axios to latest to address CVE-2023-45857

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import axiosRetry from "axios-retry";
-import { LongPoll, Socket } from "phoenix";
+import { Socket } from "phoenix";
 import { AxiosError } from "axios";
 
 type ApiClientOptions = {
